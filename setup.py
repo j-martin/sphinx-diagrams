f46358d695e54d00a505add1225adff818b25faa
Jean-Martin Archer <jm@jmartin.ca>
2020-08-20 10:49:02 -0700

    Adding sphinx diagrams extension

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sphinx-diagram",
+    version="0.0.0",
+    description="",
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=["diagrams"],
+)
